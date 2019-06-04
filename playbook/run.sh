b56217b64fe6d7b88cf78ead33ed9bc5dccba555
Wei <zero.maxima@gmail.com>
2019-06-04 23:46:36 +0800

    [Update] Add ansible.cfg to run command

--- playbook/run.sh
+++ playbook/run.sh
@@ -1,4 +1,19 @@
 #! /bin/sh
 
-ansible vagrant-target -i hosts -m ping
+# Command 1
+ ansible vagrant-target -m ping
 
+# Command 2
+ansible vagrant-target -m command -a uptime
+
+# Command 3, default command
+ansible vagrant-target -a uptime
+
+# Command 4
+ansible vagrant-target -a "tail /var/log/dpkg.log"
+
+# Command 5, as root
+ansible vagrant-target -b -a "tail /var/log/syslog"
+
+# Command 6, Install package
+ansible vagrant-target -b -m apt -a name=nginx
